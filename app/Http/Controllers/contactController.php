45486b8dfec44d6fac9a7479eef6d8f4814512bb
Arthur Rocha Gaspar <ra2357041>
2025-03-08 11:33:53 -0300

    aula4

---
+++ app/Http/Controllers/contactController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Faker\Guesser\Name;
+use Illuminate\Http\Request;
+
+class contactController extends Controller
+{
+    //
+    public function index(Request $req)
+    {
+        dd($req);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/olamundo', function(){
+    return "ola mundo";
+});
+
+Route::get('/faleconosco/{id?}', 
+function($id = null){
+    return "recebi o valor = $id";
+});
+
+Route::post('/contato', ['uses'=>'App\Http\Controllers\contactController@index']);
+
